ff99a162d62d04a244b33e76a5b24a2e8d8a8e3c
Pramuditha Karunarathna <shemil076@gmail.com>
2022-01-01 12:32:22 +0530

    OneToOne Mapping = Retrieving Student with Passport and Eager Fetch

---
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/StudentRepositoryTest.java
@@ -0,0 +1,38 @@
+package com.in28minutes.jpa.hibernate.demo.repository;
+
+import com.in28minutes.jpa.hibernate.demo.DemoApplication;
+import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.entity.Student;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= DemoApplication.class)
+class StudentRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    StudentRepository repository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    void retrieveStudentAndPassport() {
+        Student student = em.find(Student.class, 20001L);
+        logger.info("Student -> {}", student);
+        logger.info("Passport -> {}", student.getPassport());
+    }
+}
