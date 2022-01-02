ffc37875815c2a443d69aa0f44ab0961b797d09a
Pramuditha Karunarathna <shemil076@gmail.com>
2022-01-02 21:22:01 +0530

    Testing the Spring data JPA Repository with findById

---
+++ src/test/java/com/in28minutes/jpa/hibernate/demo/repository/CourseSpringDataRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.in28minutes.jpa.hibernate.demo.repository;
+
+import com.in28minutes.jpa.hibernate.demo.DemoApplication;
+import com.in28minutes.jpa.hibernate.demo.entity.Course;
+import com.in28minutes.jpa.hibernate.demo.entity.Review;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import java.util.Optional;
+
+import static junit.framework.TestCase.assertFalse;
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes= DemoApplication.class)
+class CourseSpringDataRepositoryTest {
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    @Autowired
+    CourseSpringDataRepository repository;
+
+    @Autowired
+    EntityManager em;
+
+    @Test
+    public void findById_coursePresent() {
+        Optional<Course> courseOptional = repository.findById(10001L);
+        assertTrue(courseOptional.isPresent());
+    }
+
+    @Test
+    public void findById_courseNotPresent() {
+        Optional<Course> courseOptional = repository.findById(20001L);
+        assertFalse(courseOptional.isPresent());
+    }
+}
